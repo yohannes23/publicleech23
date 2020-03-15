8d5f50409e2967717ae7224825d868aa88c2c6fc
yohannes23 <62197299+yohannes23@users.noreply.github.com>
2020-03-15 12:20:24 +0300

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -39,7 +39,7 @@ async def youtube_dl_call_back(bot, update):
     if current_user_id != current_touched_user_id:
         await bot.answer_callback_query(
             callback_query_id=update.id,
-            text="who are you? 🤪🤔🤔🤔",
+            text="ማኖት ቆይ? 🤪🤔🤔🤔",
             show_alert=True,
             cache_time=0
         )
@@ -87,14 +87,14 @@ async def youtube_dl_call_back(bot, update):
     if "noyes.in" in youtube_dl_url or "tor.checker.in" in youtube_dl_url:
         await bot.edit_message_text(
             chat_id=update.message.chat.id,
-            text="😡😡 <i>please do not abuse this <u>FREE</u> service</i> 🌚",
+            text="😡😡 <i> አታነደኝ እንግዲ <u>ዋ</u> በነፃ ሆኖ ልታበሳጨኝ</i> 🌚",
             message_id=update.message.message_id
         )
         return
-    if "drive.google.com" in youtube_dl_url and youtube_dl_format != "source":
+    if "drive.google.com" in youtube_dl_url and youtube_dl_format != "ምንጭ":
         await bot.edit_message_text(
             chat_id=update.message.chat.id,
-            text="<i>please do not abuse this <u>FREE</u> service</i>",
+            text="<i>አታነደኝ እንግዲ <u>ዋ</u> በነፃ ሆኖ ልታበሳጨኝ</i>",
             message_id=update.message.message_id
         )
         return
@@ -173,7 +173,7 @@ async def youtube_dl_call_back(bot, update):
     t_response = stdout.decode().strip()
     # LOGGER.info(e_response)
     # LOGGER.info(t_response)
-    ad_string_to_replace = "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
+    ad_string_to_replace = "ያጋጠማቹህን report አርጉኝ 😎"
     if e_response and ad_string_to_replace in e_response:
         error_message = e_response.replace(ad_string_to_replace, "")
         await bot.edit_message_text(
