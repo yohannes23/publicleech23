05783fba26fa2160413cb267af893932b5d9ffb5
yohannes23 <62197299+yohannes23@users.noreply.github.com>
2020-03-15 11:52:06 +0300

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -66,7 +66,7 @@ async def incoming_message_f(client, message):
             # if FAILED, display the error message
             await i_m_sefg.edit_text(err_message)
     else:
-        await i_m_sefg.edit_text("**FCUK**! wat have you entered. Please read /help")
+        await i_m_sefg.edit_text("**ሰላም** ያልሆነ መልእክት ነው የላኩት ")
 
 
 async def incoming_youtube_dl_f(client, message):
