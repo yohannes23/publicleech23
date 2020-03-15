e939a983e5dee24794664a716d1f275e32a93329
yohannes23 <62197299+yohannes23@users.noreply.github.com>
2020-03-15 12:07:20 +0300

    Update youtube_dl_extractor.py

--- tobrot/helper_funcs/youtube_dl_extractor.py
+++ tobrot/helper_funcs/youtube_dl_extractor.py
@@ -48,8 +48,7 @@ async def extract_youtube_dl_formats(url, user_working_dir):
     if e_response:
         # logger.warn("Status : FAIL", exc.returncode, exc.output)
         error_message = e_response.replace(
-            "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", ""
-        )
+            "ይቅርታ ምንም ነገር ከማለታቹ በፊት የላኩትን link ደጋግመው ይዩ ሌላ ችግር ካለ ቀግል አናግሩኝ)
         return error_message, None
     if t_response:
         # logger.info(t_response)
@@ -140,6 +139,6 @@ async def extract_youtube_dl_formats(url, user_working_dir):
             break
         reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
         # LOGGER.info(reply_markup)
-        succss_mesg = """Select the desired format: 👇
+        succss_mesg = """የሚፈልጉትን format: ይምረጡ 👇
 <u>mentioned</u> <i>file size might be approximate</i>"""
         return succss_mesg, reply_markup
